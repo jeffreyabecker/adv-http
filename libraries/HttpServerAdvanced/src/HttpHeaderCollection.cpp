7ca7544b6aff4dff442b3237fecb5cf000bd4d15
Jeffrey Becker <jeffrey.a.becker@gmail.com>
2026-01-24 12:04:46 -0600

    split: extract HttpHeaderCollection set/remove methods to .cpp

---
+++ libraries/HttpServerAdvanced/src/HttpHeaderCollection.cpp
@@ -0,0 +1,71 @@
+#include "HttpHeaderCollection.h"
+#include "HttpHeader.h"
+#include "StringUtility.h"
+
+namespace HttpServerAdvanced
+{
+    void HttpHeaderCollection::set(const HttpHeader &header, bool forceOverwrite)
+    {
+        set(header.name(), header.value(), forceOverwrite);
+    }
+
+    void HttpHeaderCollection::set(HttpHeader &&header, bool forceOverwrite)
+    {
+        const String &name = header.name();
+        const String &value = header.value();
+
+        auto it = std::find_if(begin(), end(), [&](const HttpHeader &h)
+                               { return h.name().equalsIgnoreCase(name); });
+
+        if (it != end())
+        {
+            if (name.equalsIgnoreCase(HttpHeaderNames::SetCookie))
+            {
+                if (forceOverwrite)
+                {
+                    *it = std::move(header);
+                }
+                else
+                {
+                    emplace_back(std::move(header));
+                }
+                return;
+            }
+            else
+            {
+                if (forceOverwrite)
+                {
+                    *it = std::move(header);
+                }
+                else
+                {
+                    *it = HttpHeader(name, it->value() + "," + value);
+                }
+            }
+        }
+        else
+        {
+            emplace_back(std::move(header));
+        }
+    }
+
+    void HttpHeaderCollection::set(const String &name, const String &value, bool forceOverwrite)
+    {
+        HttpHeader header(name, value);
+        set(std::move(header), forceOverwrite);
+    }
+
+    void HttpHeaderCollection::remove(const String &name)
+    {
+        erase(std::remove_if(begin(), end(), [&](const HttpHeader &h)
+                             { return h.name().equalsIgnoreCase(name); }),
+              end());
+    }
+
+    void HttpHeaderCollection::remove(const String &name, const String &value)
+    {
+        erase(std::remove_if(begin(), end(), [&](const HttpHeader &h)
+                             { return h.name().equalsIgnoreCase(name) && h.value().equals(value); }),
+              end());
+    }
+} // namespace HttpServerAdvanced
--- libraries/HttpServerAdvanced/src/HttpHeaderCollection.h
+++ libraries/HttpServerAdvanced/src/HttpHeaderCollection.h
@@ -65,71 +65,15 @@ namespace HttpServerAdvanced
       return it != end();
     }
 
-    void set(const HttpHeader &header, bool forceOverwrite = false)
-    {
-      set(header.name(), header.value(), forceOverwrite);
-    }
+    void set(const HttpHeader &header, bool forceOverwrite = false);
 
-    void set(HttpHeader &&header, bool forceOverwrite = true)
-    {
+    void set(HttpHeader &&header, bool forceOverwrite = true);
 
-      const String &name = header.name();
-      const String &value = header.value();
+    void set(const String &name, const String &value, bool forceOverwrite = false);
 
-      auto it = std::find_if(begin(), end(), [&](const HttpHeader &h)
-                             { return h.name().equalsIgnoreCase(name); });
+    void remove(const String &name);
 
-      if (it != end())
-      {
-        if (name.equalsIgnoreCase(HttpHeaderNames::SetCookie))
-        {
-          if (forceOverwrite)
-          {
-            *it = std::move(header);
-          }
-          else
-          {
-            emplace_back(std::move(header));
-          }
-          return;
-        }
-        else
-        {
-          if (forceOverwrite)
-          {
-            *it = std::move(header);
-          }
-          else
-          {
-            *it = HttpHeader(name, it->value() + "," + value);
-          }
-        }
-      }
-      else
-      {
-        emplace_back(std::move(header));
-      }
-    }
-
-    void set(const String &name, const String &value, bool forceOverwrite = false)
-    {
-      HttpHeader header(name, value);
-      set(std::move(header), forceOverwrite);
-    }
-
-    void remove(const String &name)
-    {
-      erase(std::remove_if(begin(), end(), [&](const HttpHeader &h)
-                           { return h.name().equalsIgnoreCase(name); }),
-            end());
-    }
-
-    void remove(const String &name, const String &value)
-    {
-      erase(std::remove_if(begin(), end(), [&](const HttpHeader &h)
-                           { return h.name().equalsIgnoreCase(name) && h.value().equals(value); }),
-            end());
-    }
+    void remove(const String &name, const String &value);
   };
 
 } // namespace HttpServerAdvanced
